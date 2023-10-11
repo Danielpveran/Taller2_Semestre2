0a42db67b5812ec323dc04b5bdee2fd587f58d5f
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-10-11 13:20:18 -0500

    Cajero en C 1.0.3
    Agrego y corrijo el caso 2 de retiro tambien corrijo algunos errores del caso 1

--- Cajerp1.cpp
+++ Cajerp1.cpp
@@ -8,22 +8,27 @@ int main(){
 		printf ("\tBienvanido a su Cajero");//el \t deja espacios
 		printf("\n1.Ingreso en la cuenta");
 		printf("\n2.Retirar");
-		
+		printf("\n3.Salir");
+		printf("\n3.Ver saldo actual");
 		scanf("%i",&opcion);
 		 
 		 switch(opcion){
 		 	
-		 	case 1://2
+		 	case 1: //agregar a cuenta
 		 	}
-		 	 printf("\nCuanto desea ingresar en cuenta?.")
-		 	 scanf("%f"&Agregar,);
-		 	 saldo = saldo += agregar;
-		 	 printf("Su saldo total es de: %f",saldo)
+			 	printf("\nCuanto desea ingresar en cuenta?.");
+			 	scanf("%f",&Agregar);
+			 	saldo += agregar;
+			 	printf("Su saldo total es de: %.2f",saldo);
 		 	break;
 		 	
 		 	case 2:
-		 	 printf("Cuanto desea retirar?")
-		 	 scanf("%f"&Retirar,);
+		 		printf("Cuanto desea retirar?")
+		 		scanf("%f"&Retirar);
+		 	 
+		 	 if (Retirar > Saldo){
+		 	 printf("Saldo Insuficiente ",saldo);
+		 	 }
 		 		
 		 	case 3:
 		 		
