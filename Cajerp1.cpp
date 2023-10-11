914c69b20a05e7c6ddf45cc6605f8e874013b24f
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-10-11 12:59:00 -0500

    Cajero en C
    Olvide conectarlo a git ub por lo que en esta etapa ya esta lista el

---
+++ Cajerp1.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+	
+	int Opcion,Saldo=1000;
+	float Agregar;
+	
+		printf ("\tBienvanido a su Cajero");//el \t deja espacios
+		printf("\n1.Ingreso en la cuenta");
+		printf("\n2.Retirar");
+		
+		scanf("%i",&opcion);
+		 
+		 switch(opcion){
+		 	
+		 	case 1://2
+		 	}
+		 	 printf("\nCuanto desea ingresar en cuenta?.")
+		 	 scanf("%f"&Agregar,);
+		 	 saldo = saldo += agregar;
+		 	 printf("Su saldo total es de: %f",saldo)
+		 	break;
+		 	
+		 	case 2:
+		 	 printf("Cuanto desea retirar?")
+		 	 scanf("%f"&Retirar,);
+		 		
+		 	case 3:
+		 		
+		 	break
+		 	
+		 	case 4;
+		 	 printf("Su saldo total es de: %f",saldo)
+		 	 
+		 	break;
+		 	
+			default: //// este Código se ejecuta si ninguno de los casos anteriores coincide con expresion siendo similar al else
+		 }
+		
+	
+	return 0;
+}
