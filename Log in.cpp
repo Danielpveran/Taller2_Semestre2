f155717ce9506e97c2e908cba6a5b550f224a82c
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-10-14 13:24:15 -0500

    Cajero Vercion final
    Se pone el sistema de retiros en la situacion donde la clave y el numero de tarjeta son correctos

--- Log in.cpp
+++ Log in.cpp
@@ -7,9 +7,12 @@
 
 
 int main()
+
 {
 	char Card_Number[30];
 	char KeyWord[30];
+	int Opcion, Saldo = 1000;
+    int Agregar, Retirar;
 	
 	int c=1; //Gona increase according to the trys of sig in with a limit of three tries
 	
@@ -24,17 +27,56 @@ int main()
 		
 		if (strcmp(Card_Number, Card_Number_Preset) == 0 && strcmp(KeyWord, KeyWord_Preset) == 0)
 		 {
-        printf("correcto\n", Card_Number);
+		while (1) {
+		        printf("\nBienvenido a su Cajero\n");
+		        printf("1. Ingreso en la cuenta\n");
+		        printf("2. Retirar\n");
+		        printf("3. Salir\n");
+		        printf("4. Ver Saldo actual\n");
+		        scanf("%i", &Opcion);
+		
+		        switch (Opcion) {
+		            case 1:
+		                printf("\nCuánto desea ingresar en cuenta? ");
+		                scanf("%i", &Agregar);
+		                Saldo += Agregar;
+		                printf("Su Saldo total es de: %i\n", Saldo);
+		                break;
+		
+		            case 2:
+		                printf("Cuánto desea retirar? ");
+		                scanf("%i", &Retirar);
+		
+		                if (Retirar > Saldo) {
+		                    printf("Saldo Insuficiente. Su saldo actual es de: %i\n", Saldo);
+		                } else {
+		                    Saldo -= Retirar;
+		                    printf("El Saldo disponible es de: %i\n", Saldo);
+		                }
+		                break;
+		
+		            case 3:
+		                // Salir del programa
+		                return 0;
+		
+		            case 4:
+		                printf("Su Saldo total es de: %i\n", Saldo);
+		                break;
+		
+		            default:
+		                printf("Ingrese una opción válida\n");
+		                break;
+		        }
         
 		}
-	 else {
-	 	printf("User/password its wrong please try again\n", Card_Number);
+	}
+	else {
+		printf("User/password its wrong please try again\n", Card_Number);
         c++;
     	}
     
     if (c > 3){
     	printf("You have reached the limit of attempts, the account number will be blocked for your safety.\n");
-    	
     	}
 	}
 	return 0;
